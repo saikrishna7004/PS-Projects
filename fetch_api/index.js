199ebf32a1edc75f049fc2fe17bc56d1ae3fd414
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2022-12-10 00:18:36 +0530

    Added bootstrap

--- fetch_api/index.js
+++ fetch_api/index.js
@@ -1,14 +1,20 @@
-fetch("https://reqres.in/api/users", {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        name: 'Test name',
-        username: 'Test-Username',
-        rollno: 'test-roll'
+function getData() {
+    fetch("https://reqres.in/api/users", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: 'Test name',
+            username: 'Test-Username',
+            rollno: 'test-roll'
+        })
+    }).then(data => data.json()).then(data => {
+        console.log(data)
+        res.innerHTML = JSON.stringify(data, undefined, 4);
+    }).catch(e=>{
+        console.log(e)
+        Swal.fire('Error', 'An error occured while fetching', 'error')
     })
-}).then(data=>data.json()).then(data=>{
-    console.log(data)
-    res.innerHTML = JSON.stringify(data, undefined, 4);
-})
+}
+getData()
