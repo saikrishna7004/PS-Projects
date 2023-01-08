e43e4cd661802bc5497d44a63021a3696ac8b67e
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2023-01-08 21:42:05 +0530

    Modified format display in table

--- json_table/index.js
+++ json_table/index.js
@@ -28,9 +28,19 @@ async function getData() {
     </thead>
     <tbody>` + d2 + `</tbody></table>`
 
+
+    let r1 = "", r2 = ""
+
     data.Employees.forEach(e => {
-        if (e.Salary > 50000 && e.DateOfJoining.substring(3, 5) == 12) {
-            console.log(e)
+        if (e.Salary > 50000) {
+            r1 += `${e.Name}<br>`
+        }
+        if (e.DateOfJoining.substring(3, 5) == 12) {
+            r2 += `${e.Name}<br>`
         }
     });
+
+    res1.innerHTML = `<h3>Salary>50000</h3><div>` + r1
+    res2.innerHTML = `<h3>Joined in December</h3><div>` + r2
+
 }
